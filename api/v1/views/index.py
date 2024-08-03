d1eb812863862f64603ae70fddd65bf52acb5e2c
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-08-03 18:43:22 +0300

    3

---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# api/v1/views/index.py
+
+from flask import jsonify
+from api.v1.views import app_views
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    return jsonify({"status": "OK"})
