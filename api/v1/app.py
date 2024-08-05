a81eff8b8197267806a709158ab82cfc51c5706c
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-08-05 09:39:30 +0300

    4.1

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
+"""Flask server (variable app)
+"""
 
 
-import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,7 +19,11 @@ def downtear(self):
     storage.close()
 
 
-if __name__ == "_main_":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,17 +8,18 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
-def returnstuff():
+@app_views.route('/status', methods=['GET'])
+def status():
     '''return stuff'''
-    return jsonify(status='OK')
+    return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
     '''JSON Responses'''
     todos = {'states': State, 'users': User,
-            'amenities': Amenity, 'cities': City,
-            'places': Place, 'reviews': Review}
+             'amenities': Amenity, 'cities': City,
+             'places': Place, 'reviews': Review}
     for key in todos:
         todos[key] = storage.count(todos[key])
     return jsonify(todos)
