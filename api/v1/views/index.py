a6091a9344892378df2f3092c7861565e24055ab
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-08-05 10:24:19 +0300

    5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 '''api status'''
-
 import models
 from models import storage
+from models.amenity import Amenity
 from models.base_model import BaseModel
 from flask import jsonify
 from api.v1.views import app_views
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'])
