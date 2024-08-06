00e4a2e58228c8c10add2709bb3dc834aa34d15c
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-08-06 04:22:49 +0300

    handle-cities+places+reviews

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,7 +7,8 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
 def get_cities(state_id):
     """ Retrieves the list of all City objects of a State """
     state = storage.get(State, state_id)
@@ -38,7 +39,8 @@ def delete_city(city_id):
     return jsonify({}, 200)
 
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def add_city(state_id):
     """Creates a city"""
     state = storage.get(State, state_id)
